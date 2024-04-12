24b0a05186a97f3c7dcbe52e4ab16b2819a5da50
yangpingkang <yangpingkangshaoxia@gmail.com>
2024-04-12 18:48:46 +0800

    Update

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -35,7 +35,7 @@ func main() {
 	if err != nil {
 		slog.Error("main", "database ping error", err.Error())
 	}
-	log.Println("db ping successfully, Just test.")
+	log.Println("db ping successfully, Just test 2.")
 
 	uh := userHandler{
 		ctx: context.Background(),
